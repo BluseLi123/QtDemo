d321ae8553bda122b274a1c8db99169cc7a97021
BluseLi123 <l870580572@qq.com>
2020-03-21 18:03:26 +0800

    2020/3/21 18:03

---
+++ 1_2_helloQt/language.ts
@@ -0,0 +1,13 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="zh" sourcelanguage="en">
+<context>
+    <name>QObject</name>
+    <message>
+        <location filename="main.cpp" line="19"/>
+        <source>Hello Qt</source>
+        <translatorcomment>你好 Qt</translatorcomment>
+        <translation>你好 Qt</translation>
+    </message>
+</context>
+</TS>
